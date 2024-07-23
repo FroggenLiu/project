9e14b6595e194455539e11fd7133e3b05c4168bc
FroggennLiu <s307226@gmail.com>
2024-07-24 00:06:19 +0800

    change name  rules

--- parseConfig.py
+++ parseConfig.py
@@ -11,21 +11,15 @@ import ipaddress
 #def sqlExecute(sql_stat: str, val: str) -> None:
 #    cursor.execute(sql_stat, val)
 #    db.commit()
-class configParse:
+class fortinet_config_parser:
     def parse_addrgrp(self, path: str) -> None:
         addrgrp_reg = r'(?P<addrgrp>.*addrgrp(.*\n)*?.*end)'
         content_reg = r'(?P<grp_name>.\"\w+.*\")(?P<set>(.*\n)*?.*next)'
         data = {}
         with open(path, 'r', encoding='utf-8') as f:
-            """
-            1. named a non-capture in regex, the name is `addrgrp` by using regex `?p<namme>`.
-            2, to capture `config firewall addrgrp` block in config using re.match to filter. and match will grouped in addrgrp group.
-            3. parse  `edit` & `set` line and insert into DB.
-            """
-            #p = re.compile(addrgrp_reg)
-            for i in re.finditer(content_reg, re.search(addrgrp_reg, f.read()).group('addrgrp')):
-                data[(i.group('grp_name').replace('"', '')).strip()] = re.sub(r'set\smember\s', '', i.group(3).strip())
-            #print(data)
+            for line in re.finditer(content_reg, re.search(addrgrp_reg, f.read()).group('addrgrp')):
+                data[(line.group('grp_name').replace('"', '')).strip()] = re.sub(r'set\smember\s', '', line.group(3).strip())
+
             for k,v in data.items():
                 for i in re.split(r'\s', v.replace('"', '')):
                     if re.match(r'^\d', i):
@@ -35,30 +29,27 @@ class configParse:
     def parse_firewall_policy(self, path: str) -> None:
         fwpolicy_block_reg = r'(?P<fw>.*firewall\spolicy(.*\n)*?.*end)'
         content_reg = r'(?P<policy_id>.edit\s\d.*)(?P<set>(.*\n)*?.*next)'
-        replace_set_next_reg = r'(.*set\s)|.*next'
-        replace_edit_reg = r'edit\s'
-        replace_prefix_reg = r'^(\w+\s|\w+\-\w+\s)'
-        replace_scape_reg = r'\n'
-        replace_apostrophe_reg = r'\"'
-        split_space_reg = r'\s'
-        split_comma_reg = r'\,'
 
         data = {}
         with open(path, 'r', encoding='utf-8') as f:
-            for id in re.finditer(content_reg, re.search(fwpolicy_block_reg, f.read()).group('fw')):
-                data[re.sub(replace_edit_reg, '', id.group('policy_id').strip())] = {}
-                for line in re.split(split_comma_reg, re.sub(replace_scape_reg, ',', re.sub(replace_set_next_reg, '', id.group('set').strip()))):
-                    if re.split(split_space_reg, line)[0]:
-                        data[re.sub(replace_edit_reg, '', id.group('policy_id').strip())][re.split(split_space_reg, line)[0]] = re.sub(replace_prefix_reg, '', line)
-        #print(data)
+            for line in re.finditer(content_reg, re.search(fwpolicy_block_reg, f.read()).group('fw')):
+                policy_id = re.sub(r'edit\s', '', line.group('policy_id').strip())
+                data[policy_id] = {}
+                for i in re.split(r'\,', re.sub(r'\n', ',', re.sub(r'(.*set\s)|.*next', '', line.group('set').strip()))):
+                    set_parameter = re.split(r'\s', i)[0]
+                    val = re.sub(r'^(\w+\s|\w+\-\w+\s)', '', i)
+                    if set_parameter:
+                        data[policy_id][set_parameter] = val
+
         for k, v in data.items():
             if 'status' not in v or v.get('status') != 'disable':
                 print(v)
-                srcintf = re.sub(replace_apostrophe_reg, '', v.get('srcintf'))
-                dstintf = re.sub(replace_apostrophe_reg, '', v.get('dstintf'))
-                
-
-                print (srcintf, dstintf)
+                srcintf = re.sub(r'\"', '', v.get('srcintf'))
+                dstintf = re.sub(r'\"', '', v.get('dstintf'))
+                srcaddr = ','.join(sip for sip in re.split(r'\s', re.sub(r'\"', '', v.get('srcaddr'))))
+                dstaddr = ','.join(dip for dip in re.split(r'\s', re.sub(r'\"', '', v.get('dstaddr'))))
+                service = ','.join(svc for svc in re.split(r'\s', re.sub(r'\"', '', v.get('service'))))
+                print (k, srcintf, dstintf, srcaddr, dstaddr, service, v.get('comments'))
 
 
                  
@@ -70,7 +61,7 @@ if __name__ == "__main__":
     #except Exception as error:
     #    print('ERROR', error)
     #db = musql.connect(host='localhost', user='', passwd=pwd, db='pcloudfw', charset='utf8')
-    sol = configParse()
+    sol = fortinet_config_parser()
     #sol.parse_addrgrp('FW3.conf')
     sol.parse_firewall_policy('FW3.conf')
     
