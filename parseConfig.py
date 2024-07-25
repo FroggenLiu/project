a9433afe19a6c1a7af6ee58553fe195bd96363e4
FroggennLiu <s307226@gmail.com>
2024-07-25 12:28:52 +0800

    add main function

--- parseConfig.py
+++ parseConfig.py
@@ -12,46 +12,48 @@ import ipaddress
 #    cursor.execute(sql_stat, val)
 #    db.commit()
 class fortinet_config_parser:
-    def parse_addrgrp(self, path: str) -> None:
+    def parse_addrgrp(self, content: str) -> None:
         addrgrp_reg = r'(?P<addrgrp>.*addrgrp(.*\n)*?.*end)'
         content_reg = r'(?P<grp_name>.\"\w+.*\")(?P<set>(.*\n)*?.*next)'
         data = {}
-        with open(path, 'r', encoding='utf-8') as f:
-            for line in re.finditer(content_reg, re.search(addrgrp_reg, f.read()).group('addrgrp')):
-                data[(line.group('grp_name').replace('"', '')).strip()] = re.sub(r'set\smember\s', '', line.group(3).strip())
+        #with open(path, 'r', encoding='utf-8') as f:
+        for line in re.finditer(content_reg, re.search(addrgrp_reg, content).group('addrgrp')):
+            data[(line.group('grp_name').replace('"', '')).strip()] = re.sub(r'set\smember\s', '', line.group(3).strip())
 
-            for k,v in data.items():
-                for i in re.split(r'\s', v.replace('"', '')):
-                    if re.match(r'^\d', i):
-                        ip, mask = re.split(r'\/', i)
-                        #TODO insert data into DB table `group` & pip install mysqldb
+        print(data)
+        for k,v in data.items():
+            for i in re.split(r'\s', v.replace('"', '')):
+                if re.match(r'^\d', i):
+                    ip, mask = re.split(r'\/', i)
+                    #TODO insert data into DB table `group` & pip install mysqldb
     
-    def parse_firewall_policy(self, path: str) -> None:
+    def parse_firewall_policy(self, content: str) -> None:
         fwpolicy_block_reg = r'(?P<fw>.*firewall\spolicy(.*\n)*?.*end)'
         content_reg = r'(?P<policy_id>.edit\s\d.*)(?P<set>(.*\n)*?.*next)'
 
         data = {}
         replacements = {
             'ALL_TCP': 'TCP/1-65535',
-            'ALL_UDP': 'UDP/1-65536',
+            'ALL_UDP': 'UDP/1-65535',
             'ALL_ICMP': 'ICMP_ANY',
             'TCP-': 'TCP/',
             'tcp': 'TCP/',
             'UDP-': 'UDP/',
             'udp': 'UDP/',
             'TCP': 'TCP/',
-            'UPD': 'UDP'
+            'UPD': 'UDP',
         }
-        with open(path, 'r', encoding='utf-8') as f:
-            for line in re.finditer(content_reg, re.search(fwpolicy_block_reg, f.read()).group('fw')):
-                policy_id = re.sub(r'edit\s', '', line.group('policy_id').strip())
-                data[policy_id] = {}
-                for i in re.split(r'\,', re.sub(r'\n', ',', re.sub(r'(.*set\s)|.*next', '', line.group('set').strip()))):
-                    set_parameter = re.split(r'\s', i)[0]
-                    val = re.sub(r'^(\w+\s|\w+\-\w+\s)', '', i)
-                    if set_parameter:
-                        data[policy_id][set_parameter] = val
 
+        #with open(path, 'r', encoding='utf-8') as f:        
+        for line in re.finditer(content_reg, re.search(fwpolicy_block_reg, content).group('fw')):
+            policy_id = re.sub(r'edit\s', '', line.group('policy_id').strip())
+            data[policy_id] = {}
+            for i in re.split(r'\,', re.sub(r'\n', ',', re.sub(r'(.*set\s)|.*next', '', line.group('set').strip()))):
+                set_parameter = re.split(r'\s', i)[0]
+                val = re.sub(r'^(\w+\s|\w+\-\w+\s)', '', i)
+                if set_parameter:
+                    data[policy_id][set_parameter] = val
+        #print(data)
         for k, v in data.items():
             if 'status' not in v or v.get('status') != 'disable':
                 #print(v.get('service'))
@@ -59,12 +61,13 @@ class fortinet_config_parser:
                 dstintf = re.sub(r'\"', '', v.get('dstintf'))
                 srcaddr = ','.join(sip for sip in re.split(r'\s', re.sub(r'\"', '', v.get('srcaddr'))))
                 dstaddr = ','.join(dip for dip in re.split(r'\s', re.sub(r'\"', '', v.get('dstaddr'))))
-                service = re.compile(r'|'.join(map(re.escape, replacements))).sub(lambda m: replacements[m.group()], ','.join(svc for svc in re.split(r'\s', re.sub(r'\"', '', v.get('service')))))
+                service = re.compile('|'.join(map(re.escape, replacements))).sub(lambda m: replacements[m.group()], ','.join(svc for svc in re.split(r'\s', re.sub(r'\"', '', v.get('service')))))
+                #re.sub('({})'.format('|'.join(map(re.escape, replacements.keys()))), lambda m: replacements[m.group()], service)
                 comments = re.sub(r'\"', '', v.get('comments')) if 'comments' in v else ''
-                #print(re.sub('({})'.format('|'.join(map(re.escape, replacements.keys()))), lambda m: replacements[m.group()], service))
+                #print(f'{k}, {srcintf}, {dstintf}, {srcaddr}, {dstaddr}, {service}, {comments}')
+                #TODO insert data into DB & pip install mysqldb
 
-        
-if __name__ == "__main__":
+def main():
     #user = input("Please give DB User name: ")
     #try :
     #    pwd = getpass.getpass('Please give DB password: ')
@@ -72,5 +75,11 @@ if __name__ == "__main__":
     #    print('ERROR', error)
     #db = musql.connect(host='localhost', user='', passwd=pwd, db='pcloudfw', charset='utf8')
     sol = fortinet_config_parser()
-    #sol.parse_addrgrp('FW3.conf')
-    sol.parse_firewall_policy('FW3.conf')
+    with open('FW3.conf', 'r', encoding='utf-8') as f:
+        content = f.read()
+    sol.parse_addrgrp(content)
+    sol.parse_firewall_policy(content)
+
+
+if __name__ == "__main__":
+    main()
