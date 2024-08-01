9d1b39480674256d4b2bb5d6d4d28028f954b19c
froggenliu <s307226@gmail.com>
2024-08-01 17:40:26 +0800

    using context manage to setup DB connection

--- parseConfig.py
+++ parseConfig.py
@@ -8,13 +8,34 @@ from mysql.connector import errorcode
 from ipaddress import IPv4Network
 from dotenv import load_dotenv
 
+add_vlan = ("INSERT INTO vlan (fwid, vname, network, cidr, vorder) VALUES (%s, %s, %s, %s, %s)")
 
-#db = musql.connect(host='localhost', user='', passwd='', db='', charset='')
-#cursor = db.cursor()
+class Database:
 
-#def sqlExecute(sql_stat: str, val: str) -> None:
-#    cursor.execute(sql_stat, val)
-#    db.commit()
+    def __init__(self):
+        load_dotenv()
+        self.config = {
+            'user': os.getenv('USER'),
+            'password': os.getenv('PASSWORD'),
+            'host': os.getenv('HOST'),
+            'database': os.getenv('DATABASE'),
+            'raise_on_warnings': True,
+            'charset': 'utf8'
+        }
+
+    def __enter__(self):
+        self.cnx = mysql.connector.connect(**self.config)
+        self.cursor = self.cnx.cursor()
+        print("DB Connection established !!")
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.cnx.commit()
+        self.cursor.close()
+        self.cnx.close()
+        print("DB Connection has been closed.")         
+
+
 class fortinet_config_parser:
 
     def parse_system_interface(self, content: str) -> dict:
@@ -33,13 +54,14 @@ class fortinet_config_parser:
         #print(data)
         return data
 
-    def parse_system_zone(self, content: str) -> None:
+    def parse_system_zone(self, db: mysql.connector.cursor, content: str) -> None:
         '''
             1.parse `zone` in config and mapping zone interface
         '''
         system_zone_block_reg = r'(?P<syszone>.*system\szone(.*\n)*?.*(?<=next\n)end)'
         content_reg = r'(?P<zone>\".*\")(?P<set>(.*\n)*?.*next)'
         interfcae_dict = self.parse_system_interface(content)
+        order = 0
 
         for line in re.finditer(content_reg, re.search(system_zone_block_reg, content).group('syszone')):
             for i in re.split(r',', re.sub(r'\n', ',', re.sub(r'.*(set\s|next|end|config\s.*)', '', line.group('set').strip()).strip())):
@@ -49,12 +71,12 @@ class fortinet_config_parser:
                         #{'interface', 'ip'} <= (interfcae_dict[interface]).keys():
                         if interface in interfcae_dict and all(k in interfcae_dict[interface] for k in ('vdom', 'ip')):
                             #print(interface, interfcae_dict[interface]['vdom'][0], interfcae_dict[interface]['interface'],  interfcae_dict[interface]['ip'])
-                            #TODO insert data into `vlan` table
+                            order += 1
                             address, netmask = re.split(r'\/' ,str(IPv4Network('/'.join(ip_mask for ip_mask in interfcae_dict[interface]['ip']), False)))
-                            print(interface, interfcae_dict[interface]['vdom'][0], address, netmask)
-                            #insert_data = (fwid, interface, address, netmask, order)
-        
-
+                            vdom =  interfcae_dict[interface]['vdom'][0]
+                            data_vlan = ('bssFW1', interface, address, int(netmask), order)
+                            db.execute(add_vlan, data_vlan)
+                                  
     def parse_firewall_address(self, content: str) -> dict:
         fwaddress_block_reg = r'(?P<addr>.*firewall\saddress(.*\n)*?.*end\n)'
         content_reg = r'(?P<address_name>\".*\")(?P<set>(.*\n)*?.*next)'
@@ -67,7 +89,6 @@ class fortinet_config_parser:
                 data[address_obj_name][attr] = val
         #print(data)
         return data
-
 
     def parse_addrgrp(self, content: str) -> None:
         addrgrp_reg = r'(?P<addrgrp>.*addrgrp(.*\n)*?.*end)'
@@ -88,7 +109,6 @@ class fortinet_config_parser:
 
                 #if re.match(r'^\d', i):
                     #ip, mask = re.split(r'\/', i)
-
 
     def parse_firewall_policy(self, content: str) -> None:
         fwpolicy_block_reg = r'(?P<fw>.*firewall\spolicy(.*\n)*?.*end)'
@@ -132,47 +152,16 @@ class fortinet_config_parser:
                 #TODO insert data into DB 
 
 def main():
-    #user = input("Please give DB User name: ")
-    #try :
-    #    pwd = getpass.getpass('Please give DB password: ')
-    #except Exception as error:
-    #    print('ERROR', error)
-    #db = musql.connect(host='localhost', user='', passwd=pwd, db='pcloudfw', charset='utf8')
-
-    load_dotenv()
-    config = {
-        'user': os.getenv('USER'),
-        'password': os.getenv('PASSWORD'),
-        'host': os.getenv('HOST'),
-        'database': os.getenv('DATABASE'),
-        'raise_on_warnings': True,
-        'charset': 'utf8'
-    }
     
-    try:
-        cnx = mysql.connector.connect(**config)
-        cursor = cnx.cursor()
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR: 
-            print("Database does not exist")
-        else:
-            print(err)
-    else:
+    with Database() as db:
+        with open('FW1.conf', 'r', encoding='utf-8') as f:
+            content = f.read()
         forti = fortinet_config_parser()
-        with cnx.cursor() as cursor:
-            cursor.execute("SELECT * FROM user")
-            for row in cursor.fetchall():
-                print(row)
-            with open('FW1.conf', 'r', encoding='utf-8') as f:
-                content = f.read()
-            #forti.parse_system_zone(content)
-            #forti.parse_firewall_address(content)
-            #forti.parse_system_interface(content)
-            #forti.parse_addrgrp(content)
-            #forti.parse_firewall_policy(content)
-        cnx.close()
+        forti.parse_system_zone(db, content)
+        #forti.parse_firewall_address(content)
+        #forti.parse_system_interface(content)
+        #forti.parse_addrgrp(content)
+        #forti.parse_firewall_policy(content)
 
 if __name__ == "__main__":
     main()
