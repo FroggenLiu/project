858ec4672c3a876fcf5088b1caabcf13d167586b
froggenliu <s307226@gmail.com>
2024-08-23 10:38:27 +0800

    db setup be a class.

--- createUser.py
+++ createUser.py
@@ -1,41 +1,17 @@
-import bcrypt
-import mysql.connector
 import os
 import json
-from dotenv import load_dotenv
+import bcrypt
+import dbsetup
 
 add_user = ("INSERT INTO user (uid, permission, name, passwd) VALUES (%s, %s, %s, %s)")
-
-class database:
-    def __init__(self):
-        load_dotenv()
-        self.config = {
-            'user': os.getenv('USER'),
-            'password': os.getenv('PASSWORD'),
-            'host': os.getenv('HOST'),
-            'database': os.getenv('DATABASE'),
-            'raise_on_warnings': True,
-            'charset': 'utf8'
-        }
-
-    def __enter__(self):
-        self.cnx = mysql.connector.connect(**self.config)
-        self.cursor = self.cnx.cursor()
-        print("DB Connection established.")
-        return self.cursor
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.cnx.commit()
-        self.cursor.close()
-        self.cnx.close()
-        print("DB Connection has been closed.")        
-
+      
 def main():
-    load_dotenv()
-    with database() as db:
+    database = dbsetup.database()
+    with database as db:
         for k,v in json.loads(os.getenv("USERS")).items():
             hashed = bcrypt.hashpw(v[1].encode(), bcrypt.gensalt())
             data = (k, '1', v[0], hashed)
+            print(f'User \"{k}\" has been insert into database table \'user\'.')
             db.execute(add_user, data)
 
 if __name__ == "__main__":
---
+++ dbsetup.py
@@ -0,0 +1,27 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+class database:
+    def __init__(self):
+        load_dotenv()
+        self.config = {
+            'user': os.getenv('USER'),
+            'password': os.getenv('PASSWORD'),
+            'host': os.getenv('HOST'),
+            'database': os.getenv('DATABASE'),
+            'raise_on_warnings': True,
+            'charset': 'utf8'
+        }
+
+    def __enter__(self):
+        self.cnx = mysql.connector.connect(**self.config)
+        self.cursor = self.cnx.cursor()
+        print("DB Connection established.")
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.cnx.commit()
+        self.cursor.close()
+        self.cnx.close()
+        print("DB Connection has been closed.")        
