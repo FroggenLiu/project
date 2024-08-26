27152ef72d1f4d6c4254d07e68152caba81ced3d
froggenliu <s307226@gmail.com>
2024-08-26 14:32:06 +0800

    add eip code name from config

--- createFW.py
+++ createFW.py
@@ -11,20 +11,15 @@ create_table = ("CREATE TABLE {} SELECT * FROM FW3 WHERE 1 = 0")
 def add_fw_info(db: "db object"):
     for k, v in json.loads(os.getenv("FW")).items():
         hashed = bcrypt.hashpw(v.get('password').encode(), bcrypt.gensalt())
-        data = (k, v.get('vdom'), v.get('name'), v.get('ip'), v.get('account'), hashed, '0', None)
+        data = (k, v.get('vdom'), v.get('name'), v.get('ip'), v.get('account'), hashed, '0', v.get('chtdeip'))
         try:
             db.execute(add_fw, data)
             print(f'FW \"{k}\" info has been added.')
-            create_fw_table(db, k)
+            db.execute(create_table.format(fw_name))
+            print(f'Table \"{fw_name}\" has been created.')
+            
         except mysql.connector.Error as e:
             print(e)
-
-def create_fw_table(db: "db object", fw_name: str):
-    try:
-        db.execute(create_table.format(fw_name))
-        print(f'Table \"{fw_name}\" has been created.')
-    except mysql.connector.Error as e:
-        print(e)
 
 def main():
     database = dbsetup.database()
