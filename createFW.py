16a6f20c1c497501f725ed2ed36b66169ff5f727
froggenliu <s307226@gmail.com>
2024-08-27 18:10:09 +0800

    modify sql statment

--- createFW.py
+++ createFW.py
@@ -5,8 +5,11 @@ import dbsetup
 import mysql.connector 
 
 add_fw_statement = ("INSERT INTO fwinfo (fwid, vdom, name, ip, account, pw, ftype, chtdeip) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
-create_table_statment = ("CREATE TABLE {} SELECT * FROM FW3 WHERE 1 = 0")
-
+create_table_statment = (
+    "CREATE TABLE {} (`fwid` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY, `vlanfrom` varchar(50) NOT NULL, `vlanto` varchar(50) NOT NULL,"
+    "`userid` varchar(50) NOT NULL, `adminid` varchar(50) NOT NULL, `src` text NOT NULL, `dst` text NOT NULL, `service` text NOT NULL,"
+    "`comment` longtext NOT NULL, `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `nat` varchar(50)) ENGINE=InnoDB"
+    )
 
 def add_fw_info(db: "db object"):
     for k, v in json.loads(os.getenv("FW")).items():
@@ -15,9 +18,8 @@ def add_fw_info(db: "db object"):
         try:
             db.execute(add_fw_statement, data)
             print(f'FW \"{k}\" info has been added.')
-            db.execute(create_table_statment.format(fw_name))
-            print(f'Table \"{fw_name}\" has been created.')
-            
+            db.execute(create_table_statment.format(k))
+            print(f'Table \"{k}\" has been created.')
         except mysql.connector.Error as e:
             print(e)
 
